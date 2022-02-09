e2d794362a3d66108c2cbe0bd95eb26f09e1b5c4
Jean Etchichury <jean.etchichury@hotmail.com>
2022-02-09 17:20:31 -0300

    mod 2 a 1

--- forca.py
+++ forca.py
@@ -3,6 +3,23 @@ def jogar():
     print("Bem vindo ao jogo de forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("qual a letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("encontrei a letra {} na posição {}".format(letra, index+1))
+            index = index+1
+
+        print("jogando...")
+
     print("Fim do jogo")
 
 if __name__ == "__main__":
